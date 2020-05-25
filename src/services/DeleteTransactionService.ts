2b7fd4ec454e12ac652584d431701b89557b6e5a
Dahan Schuster <dan.plschuster@gmail.com>
2020-05-25 13:05:38 -0300

    Cria o serviço de deleção de transações cadastradas

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,9 +1,22 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
+import { getCustomRepository } from "typeorm";
+import TransactionsRepository from "../repositories/TransactionsRepository";
+
+interface RequestDTO {
+	transactionId: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
-  }
+	public async execute({ transactionId }: RequestDTO): Promise<void> {
+		const transactionsRepository = getCustomRepository(TransactionsRepository)
+
+		const transaction = await transactionsRepository.findOne(transactionId)
+		if (!transaction) {
+			throw new AppError('Transaction not found', 404)
+		}
+
+		await transactionsRepository.delete(transactionId)
+	}
 }
 
 export default DeleteTransactionService;
