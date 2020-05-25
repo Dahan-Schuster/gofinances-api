c7f18d36a0f327fc9c0d0fcf0e5fe7efe29aa932
Dahan Schuster <dan.plschuster@gmail.com>
2020-05-25 13:09:56 -0300

    Cria o repositório de transações
    O repositório contém o método para lisagem de todas as transações
    que retorna um array de duas posições ([0] a lista de transações e
    [1] o objeto 'balance') e o método de cálculo do objeto 'balance'

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -3,16 +3,50 @@ import { EntityRepository, Repository } from 'typeorm';
 import Transaction from '../models/Transaction';
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+	income: number;
+	outcome: number;
+	total: number;
+}
+
+interface Transactions {
+	transactions: Transaction[],
+	balance: Balance
 }
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<Balance> {
-    // TODO
-  }
+	public async getBalance(transactions?: Transaction[]): Promise<Balance> {
+		if (!transactions)
+			transactions = await this.find();
+
+		let income = 0, outcome = 0;
+		transactions.forEach(transaction => {
+			if (transaction.type == 'income') {
+				income += Number.parseFloat(String(transaction.value))
+			} else {
+				outcome += Number.parseFloat(String(transaction.value))
+			}
+		})
+
+		const total = income - outcome;
+
+		return {
+			income,
+			outcome,
+			total,
+		}
+
+	}
+
+	public async getAll(): Promise<Transactions> {
+		const transactions = await this.find({ relations: ["category"] });
+		const balance = await this.getBalance(transactions);
+
+		return {
+			transactions,
+			balance
+		}
+	}
 }
 
 export default TransactionsRepository;
